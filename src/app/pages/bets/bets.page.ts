95626c4d7a8156dbd925ecfe870f8df0d3dddbeb
harry <harrispap03@gmail.com>
2021-11-23 13:36:00 +0200

    switched bets and home page

--- src/app/pages/bets/bets.page.ts
+++ src/app/pages/bets/bets.page.ts
@@ -1,42 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonInfiniteScroll, ModalController } from '@ionic/angular';
-import { NewBetPage } from 'src/app/components/new-bet/new-bet.page';
-import { BetService } from 'src/app/services/bet.service';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-bets',
   templateUrl: 'bets.page.html',
   styleUrls: ['bets.page.scss'],
 })
-export class BetsPage implements OnInit {
-  @ViewChild(IonInfiniteScroll) inifiniteScroll: IonInfiniteScroll;
-  constructor(
-    public modalController: ModalController,
-    public betService: BetService
-  ) {}
-
-  ngOnInit() {
-    this.betService.init('bets', 'createdAt', {
-      reverse: true,
-      prepend: false,
-    });
-  }
-  async popup() {
-    const modal = await this.modalController.create({
-      component: NewBetPage,
-    });
-    return await modal.present();
-  }
-
-  loadData(event) {
-    setTimeout(() => {
-      this.betService.more();
-      event.target.complete(); // Why do I have to call this ?
-    }, 500);
-  }
-
-  toggleInfiniteScroll() {
-    this.inifiniteScroll.disabled = !this.inifiniteScroll.disabled;
-    console.log('Toggled');
-  }
+export class BetsPage {
+  constructor() {}
 }
